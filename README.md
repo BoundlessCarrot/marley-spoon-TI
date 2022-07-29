eda0d3b016bc2e42b64bb94075cd2c39f38adc9f
BoundlessCarrot <jordan.streete@gmail.com>
2022-07-29 08:45:52 +0300

    documentation, readme, and tags fix

---
+++ README.md
@@ -0,0 +1,18 @@
+# Marley Spoon Technical Interview
+
+To run this application, simply run `python3 fetch.py`, and navigate to [127.0.0.1:5000/](127.0.0.1:5000/) in your eb browser of choice
+
+I've also included a virtual environment with all of the python modules and the version of python that I used for this project. They are, in no particular order:
+
+1. [Python](https://www.python.org/downloads/release/python-395/) 3.9.5
+2. [Contentful](https://pypi.org/project/contentful/) 1.13.1
+3. [Flask](https://pypi.org/project/Flask/) 2.1.3
+4. [Markdown](https://pypi.org/project/Markdown/) 3.4.1
+
+## Notes
+
+* I would've rather put the API credentials into environment variables instead of a JSON file, but I haven't ever really worked with `.env` files before, so I decided to stick with what I know rather than try too many new things
+* The way that the markdown is parsed is pretty unsafe, from my understanding. In order to make everything work, the markdown has to be converted in the backend, and passed to the frontend as HTML. The frontend then uses the `{{<var>|safe}}` keyword in order to actually process the passed HTML. This requires that we _absolutely trust_ the information being passed into the backend. In this microcosm we know what will be passed to the application, but in the real-world this is probably an infeasible solution (to this part of the problem)
+* Currently, it seems that some recipes have a chef or tags entry while others don't. I decided to give the truth to the frontend in these cases - these portions of the webpage will simply read "Chef name unavailable!" or "No tags available!", respectively. In terms of personal philosophy/design, I'd rather display _incomplete_ information than guess at a chef or tags and display _incorrect_ information
+* I would've liked to use `async` to grab data from the API and make the whole thing faster (I also remember reading about a sync function in the contentful module) but I didn't want to mess with it considering the time pressure. If I had more time, I would definitely try to figure that out either first or second
+* Finally, just for fun, you guys almost got a very frustrating and unreadable lambda function to resolve an ID from the name of a recipe (just as a string)! I'm very happy I figured out how to change it to something much better and easier to understand, but if you want to check it out it should be in one of the past (before the final 2) commits :-)
--- fetch.py
+++ fetch.py
@@ -1,12 +1,18 @@
-import contentful as cf
 import json
-from flask import Flask, render_template
+
+import contentful as cf
 import markdown as md
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-def get_credentials() -> dict:
+def get_credentials() -> cf.Client:
+    '''
+    Gets the contentful API credentials from a JSON file in the current working directory.
+
+    Returns: client object that can be used to access the API
+    '''
     with open("credentials.json", "r") as fp:
         credentials = json.load(fp)
 
@@ -18,11 +24,18 @@ def get_credentials() -> dict:
 
 
 @app.route("/")
-def get_all_recipes() -> list:
+def get_all_recipes():
+    '''
+    Renders a webpage with a list of all available recipes, including their respective images and IDs
+
+    Returns: technically a string, but realistically the rendered webpage using the template html file specified
+    '''
     client = get_credentials()
+
+    # Creates a tuple with each recipe's name, asset url and ID, which gets passed to the frontend
     recipes = [
-        (x.title, client.asset(x.photo.id).url(), x.id)
-        for x in client.entries({"content_type": "recipe"})
+        (entry.title, client.asset(entry.photo.id).url(), entry.id)
+        for entry in client.entries({"content_type": "recipe"})
     ]
 
     return render_template("recipes.html", recipes=recipes)
@@ -30,6 +43,12 @@ def get_all_recipes() -> list:
 
 @app.route("/<id>")
 def get_detailed_view(id=None):
+    '''
+    Renders a webpage with detailed information about a single recipe
+    :param: id: str = The ID of the recipe, used to resolve all info for the recipe. We usually get this from the URL, but it can be tested if you know the ID and pass it to the function
+
+    Returns: technically a string, but realistically the rendered webpage using the template html file specified
+    '''
     client = get_credentials()
 
     # Get the specific entry we want, using the ID
@@ -38,19 +57,21 @@ def get_detailed_view(id=None):
     # Flask was complaining that tags was uninitialized, so we init it here
     tags = []
 
-    # This JSON schema isn't identical across entries, so we account for some possible errors here
+    # This JSON schema isn't uniform across entries, so we account for some possible errors here
     try:
-        chef = target_entry.fields()['chef'].name
-        tags = [tag.name for tag in target_entry.tags]
-    except KeyError:
-        chef = "Chef name unavailable!"
+        tags = [str(tag.name) + ", " for tag in target_entry.tags]
     except AttributeError:
         tags = ["No tags available!"]
+    
+    try:
+        chef = target_entry.fields()['chef'].name
+    except KeyError:
+        chef = "Chef name unavailable!"
 
     return render_template(
         "detailed_view.html", 
         recipe_title=target_entry.title, 
-        tags=tags, 
+        tags=tags,
         image=client.asset(target_entry.photo.id).url(),
         description=md.markdown(target_entry.description),
         chef=chef
@@ -58,4 +79,4 @@ def get_detailed_view(id=None):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
